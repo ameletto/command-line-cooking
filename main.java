2b0d097d32cc7b78932c399ac898ac7558af90bd
ameletto <meamymli@gmail.com>
2021-10-26 14:41:47 -0400

    Completed some options.

--- main.java
+++ main.java
@@ -4,146 +4,264 @@ import java.util.Hashtable;
 // inclued methods & arrays
 
 class Main {
-	
-	// private static final Hashtable<String, Long> tasks = new Hashtable<String, Long>();
-	// 	/**
-	// 	 * Start the Watch for a Task with Id
-	// 	 * 
-	// 	 * @param id
-	// 	 */
-	// 	public static void start(String taskId) {
-	// 		tasks.put(taskId, new Long(System.currentTimeMillis()));
-	// 	}
 
-	// 	/**
-	// 	 * Stop the watch
-	// 	 * 
-	// 	 * @param id
-	// 	 * @return
-	// 	 */
-	// 	public static long stop(String taskId) {
-	// 		return System.currentTimeMillis()
-	// 				- ((Long) tasks.remove(taskId)).longValue();
-	// 	}
+	// private static final Hashtable<String, Long> tasks = new Hashtable<String,
+	// Long>();
+	// /**
+	// * Start the Watch for a Task with Id
+	// *
+	// * @param id
+	// */
+	// public static void start(String taskId) {
+	// tasks.put(taskId, new Long(System.currentTimeMillis()));
+	// }
 
-		public static void mainMenu(){
-			System.out.println("=========================================================================================");
+	// /**
+	// * Stop the watch
+	// *
+	// * @param id
+	// * @return
+	// */
+	// public static long stop(String taskId) {
+	// return System.currentTimeMillis()
+	// - ((Long) tasks.remove(taskId)).longValue();
+	// }
+
+	public static void mainMenu() {
+		System.out.println("=========================================================================================");
+		System.out.println();
+		System.out.println("Welcome to... COMMAND-LINE");
+		System.out.println();
+		System.out.println("       _..._       .-'''-.        .-'''-.                                          ___   ");
+		System.out.println(
+				"    .-'_..._''.   '   _    \\     '   _    \\                                     .'/   \\  ");
+		System.out.println(
+				"  .' .'      '.\\/   /` '.   \\  /   /` '.   \\     .     .--.   _..._            / /     \\ ");
+		System.out
+				.println(" / .'          .   |     \\  ' .   |     \\  '   .'|     |__| .'     '.   .--./) | |     | ");
+		System.out.println(". '            |   '      |  '|   '      |  '.'  |     .--..   .-.   . /.''\\  | |     | ");
+		System.out.println(
+				"| |            \\    \\     / / \\    \\     / /<    |     |  ||  '   '  || |  | | |/`.   .' ");
+		System.out.println("| |             `.   ` ..' /   `.   ` ..' /  |   | ____|  ||  |   |  | \\`-' /   `.|   | ");
+		System.out
+				.println(". '                '-...-'`       '-...-'`   |   | \\ .'|  ||  |   |  | /(\"'`     ||___|  ");
+		System.out
+				.println(" \\ '.          .                             |   |/  . |  ||  |   |  | \\ '---.   |/___/  ");
+		System.out.println(
+				"  '. `._____.-'/                             |    /\\  \\|__||  |   |  |  /'\"\"'.\\  .'.--.  ");
+		System.out
+				.println("    `-.______ /                              |   |  \\  \\   |  |   |  | ||     ||| |    | ");
+		System.out.println(
+				"             `                               '    \\  \\  \\  |  |   |  | \\'. __// \\_\\    / ");
+		System.out.println("                                            '------'  '---''--'   '--'  `'---'   `''--' ");
+		System.out.println();
+		System.out.println("Full screen your terminal for the best experience.");
+		System.out.println();
+		System.out.println("You are at the main menu.");
+		System.out.println("========================");
+		System.out.print("Press any key to start: ");
+	}
+
+	public static void strawbShort() {
+		String[] fridgeInventory = { "Strawberries", "Eggs", "Whole milk", "Unsalted butter", "Mint sprigs",
+				"Heavy cream" };
+		String[] pantryInventory = { "Cake flour", "Sugar" };
+		String[] cupboardInventory = { "Bowl", "Cake pan" };
+		boolean hasBowl = false;
+		boolean hasCakePan = false;
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Press [0] to go back");
+		System.out.println("");
+		System.out.println("======================================================================");
+		System.out.println("|                                                                     |");
+		System.out.println("| [1] Fridge                                                          |");
+		System.out.println("| [2] Freezer                     [3] Oven                            |");
+		System.out.println("|                                               [4] Cupboard          |");
+		System.out.println("|                                                                     |");
+		System.out.println("|                  [5] Pantry                                         |");
+		System.out.println("|                                                                     |");
+		System.out.println("|                                [6] Sink                             |");
+		System.out.println("|                                                         [7] Garbage |");
+		System.out.println("|=====================================================================|");
+		System.out.println();
+		System.out.println("You are in the kitchen.");
+		System.out.println("Where would you like to go first? Press the number that corresponds with the location.");
+
+		String uInput = input.nextLine();
+		if (uInput.equals("0")) {
+			Main.mainMenu();
+		} 
+		else if (uInput.equals("1")) {
+			System.out.println("[0] Main menu");
 			System.out.println();
-			System.out.println("Welcome to... COMMAND-LINE");
-			System.out.println();
-			System.out.println("       _..._       .-'''-.        .-'''-.                                          ___   ");
-			System.out.println("    .-'_..._''.   '   _    \\     '   _    \\                                     .'/   \\  ");
-			System.out.println("  .' .'      '.\\/   /` '.   \\  /   /` '.   \\     .     .--.   _..._            / /     \\ ");
-			System.out.println(" / .'          .   |     \\  ' .   |     \\  '   .'|     |__| .'     '.   .--./) | |     | ");
-			System.out.println(". '            |   '      |  '|   '      |  '.'  |     .--..   .-.   . /.''\\  | |     | ");
-			System.out.println("| |            \\    \\     / / \\    \\     / /<    |     |  ||  '   '  || |  | | |/`.   .' ");
-			System.out.println("| |             `.   ` ..' /   `.   ` ..' /  |   | ____|  ||  |   |  | \\`-' /   `.|   | ");
-			System.out.println(". '                '-...-'`       '-...-'`   |   | \\ .'|  ||  |   |  | /(\"'`     ||___|  ");
-			System.out.println(" \\ '.          .                             |   |/  . |  ||  |   |  | \\ '---.   |/___/  ");
-			System.out.println("  '. `._____.-'/                             |    /\\  \\|__||  |   |  |  /'\"\"'.\\  .'.--.  ");
-			System.out.println("    `-.______ /                              |   |  \\  \\   |  |   |  | ||     ||| |    | ");
-			System.out.println("             `                               '    \\  \\  \\  |  |   |  | \\'. __// \\_\\    / ");
-			System.out.println("                                            '------'  '---''--'   '--'  `'---'   `''--' ");
-			System.out.println();
-			System.out.println("Full screen your terminal for the best experience.");
-			System.out.println();
-			System.out.println("You are at the main menu.");
-			System.out.println("========================");
-			System.out.print("Press any key to start: ");
+			System.out.println("==============");
+			for (int i = 0; i < fridgeInventory.length; i++) {
+				System.out.println("[" + (i + 1) + "] " + fridgeInventory[i]);
+			}
+			System.out.println("==============");
+			System.out.println("You are at the fridge.");
+			if (hasBowl == false) {
+				System.out.println("Hint: you need something to put your ingredients in.");
+			} 
+			else {
+				while (true) {
+					System.out.println("Which ingredient would you like to put in your bowl? Press the number that corresponds with the ingredient. Press [D] when done.");
+					uInput = input.nextLine();
+					if (uInput.equals("1")) {
+						System.out.println("Strawberries... in the bowl!");
+					} else if (uInput.equals("2")) {
+						System.out.println("Eggs... in the bowl!");
+					} else if (uInput.equals("3")) {
+						System.out.println("Whole milk... in the bowl!");
+					} else if (uInput.equals("4")) {
+						System.out.println("Unsalted butter... in the bowl!");
+					} else if (uInput.equals("5")) {
+						System.out.println("Mint sprigs... in the bowl!");
+					} else if (uInput.equals("6")) {
+						System.out.println("Heavy cream... in the bowl!");
+					} else if (uInput.equals("d")) {
+						Main.strawbShort();
+						break;
+					} else {
+						System.out.println("You put nothing in your bowl. lol get rekt.");
+					}
+				}
+			}
 		}
-
-		public static void strawbShort(){
-			String[] fridgeInventory = {"Strawberries", "Eggs", "Whole milk", "Unsalted butter", "Mint sprigs", "Heavy cream"};
-			String[] pantryInventory = {"Cake flour", "Sugar"};
-			String[] cupboardInventory = {"Bowl", "Cake pan"};
-			String[] sinkInventory = {"Water"};
-			boolean hasBowl = false;
-			Scanner input = new Scanner(System.in);
-			
-			System.out.println("======================================================================");
-			System.out.println("|                                                                     |");
-			System.out.println("| [1] Fridge                      [3] Stovetop                        |");
-			System.out.println("| [2] Freezer                     [4] Oven                            |");
-			System.out.println("|                                               [5] Cupboard          |");
-			System.out.println("|                                                                     |");
-			System.out.println("|                  [6] Pantry                                         |");
-			System.out.println("|                                                                     |");
-			System.out.println("|                                [7] Sink                             |");
-			System.out.println("|                                                         [8] Garbage |");
-			System.out.println("|=====================================================================|");
-			System.out.println();
-			System.out.println("You are in the kitchen.");	
-			System.out.println("Where would you like to go first? Press the number that corresponds with the location.");
-			
-			String uInput = input.nextLine();
-			if(uInput.equals("1")){
-				System.out.println("==============");
-				for(int i=0; i<inventory.length; i++){
-					System.out.println("["+(i+1)+"] "+inventory[i]);
-				}
-				System.out.println("==============");
-				System.out.println("You are at the fridge.");
-
-				System.out.println("Which ingredient would you like to use first? Press the number that corresponds with the ingredient");
+	else if(uInput.equals("2"))
+	{
+		System.out.println("You are at the freezer.");
+		System.out.println("There's nothing in here that you need.");
+		System.out.println("Press any key to go back.");
+		uInput = input.nextLine();
+		Main.strawbShort();
+	}
+	else if(uInput.equals("3"))
+	{
+		// have putting cake in oven graphics
+		System.out.println("[0] Main menu");
+		System.out.println();
+		System.out.println("You are at the oven.");
+		if(hasCakePan == false){
+			System.out.println("Hint: you need something to bake your cake in. Press [0] to go back.");
+			while(true){
 				uInput = input.nextLine();
-				if(uInput.equals("1")){
+				if (uInput.equals("0")){
+					Main.strawbShort();
+					break;
+				}
+				else{
+					System.out.println("You literally have nothing to do here, please go back.");
 				}
 			}
-			else if(uInput.equals("2")){
-				System.out.println("You are at the freezer.");
-				System.out.println("You can go to the kitchen or the pantry.");
-				System.out.println("Where would you like to go first? Press the number that corresponds with the location.");
-				uInput = input.nextLine();
-			}
-			else if(uInput.equals("3")){
-				System.out.println("You are at the stovetop.");
-				System.out.println("You can go to the kitchen or the pantry.");
-				System.out.println("Where would you like to go first? Press the number that corresponds with the location.");
-				uInput = input.nextLine();
-			}
-			else if(uInput.equals("4")){
-				System.out.println("You are at the oven.");
-				System.out.println("You can go to the kitchen or the pantry.");
-				System.out.println("Where would you like to go first? Press the number that corresponds with the location.");
-				uInput = input.nextLine();
-			}
-			else if(uInput.equals("5")){
-				System.out.println("==============");
-				for(int i=0; i<inventory.length; i++){
-					System.out.println("["+(i+1)+"] "+inventory[i]);
+		}
+		else{
+			while(true){
+				System.out.println("Would you like to put your cake in the oven? Press [Y] if yes or [N] if no.");
+				while(true){
+					uInput = input.nextLine();
+					if(uInput.equals("y")){
+						System.out.println("Cake... in the oven!");
+						try {
+							Thread.sleep(1000);
+						} catch(InterruptedException e) {
+							System.out.println(":D");
+						}
+						System.out.println("20min left");
+						try {
+							Thread.sleep(1000);
+						} catch(InterruptedException e) {
+							System.out.println(":D");
+						}
+						System.out.println("10min left");
+						try {
+							Thread.sleep(1000);
+						} catch(InterruptedException e) {
+							System.out.println(":D");
+						}
+						System.out.println("5min left");
+						try {
+							Thread.sleep(1000);
+						} catch(InterruptedException e) {
+							System.out.println(":D");
+						}
+						System.out.println("1min left");
+						try {
+							Thread.sleep(1000);
+						} catch(InterruptedException e) {
+							System.out.println(":D");
+						}
+						System.out.println("Congrats! Your cake has succesfully finished baking. Now it's time to decorate :P");
+						try {
+							Thread.sleep(1000);
+						} catch(InterruptedException e) {
+							System.out.println(":D");
+						}
+						Main.strawbShort();
+						break;
+					}
+					else if(uInput.equals("n")){
+						System.out.println("Okay then. Press [0] to go back");
+						while(true){
+							uInput = input.nextLine();
+						if(uInput.equals("0")){
+							Main.strawbShort();
+							break;
+						}
+						else{
+							System.out.println("??? Please check your reading and try again.");
+						}
+						}
+						}
+					}
 				}
-				System.out.println("You are at the cupboard.");
-				System.out.println("==============");
-				System.out.println("You are at the fridge.");
-				System.out.println("Which ingredient would you like to use first? Press the number that corresponds with the ingredient");
-				uInput = input.nextLine();
-				System.out.println("Bowls
-				Cake pan");
 				
-				System.out.println("Where would you like to go first? Press the number that corresponds with the location.");
-				uInput = input.nextLine();
-			}
-			else if(uInput.equals("6")){
-				System.out.println("You are in the pantry.");
-				System.out.println("You can go to the kitchen or the pantry.");
-				System.out.println("Where would you like to go first? Press the number that corresponds with the location.");
-				uInput = input.nextLine();
-			}
-			else if(uInput.equals("7")){
-				System.out.println("You are at the sink.");
-				System.out.println("You can go to the kitchen or the pantry.");
+	}}}
+	else if(uInput.equals("5"))
+	{
+		System.out.println("==============");
+		for (int i = 0; i < cupboardInventory.length; i++) {
+			System.out.println("[" + (i + 1) + "] " + cupboardInventory[i]);
+		}
+		System.out.println("==============");
+		System.out.println("You are at the cupboard.");
+		System.out.println("Which container would you like to use first? Press the number that corresponds with the container.");
+		uInput = input.nextLine();
+		if(uInput.equals("1")){
+			hasBowl = true;
+		}
+		else if(uInput.equals("2")){
+			hasCakePan = true;
+		}
+		else{
+			System.out.println("Try again");
+		}
+	}else if(uInput.equals("6"))
+	{
+		System.out.println("==============");
+		for (int i = 0; i < pantryInventory.length; i++) {
+			System.out.println("[" + (i + 1) + "] " + pantryInventory[i]);
+		}
+		System.out.println("==============");
+		System.out.println("You are in the pantry.");
+		System.out.println("Which container would you like to use first? Press the number that corresponds with the container.");
+		uInput = input.nextLine();
+	}else if(uInput.equals("7"))
+	{
+		System.out.println("You are at the sink.");
+		System.out.println("Would you like some water? Press [Y] if yes or [N] if no.");
+	}}
+
+	public static void chocChipCook(){
 
 		}
 
-		public static void chocChipCook(){
+	public static void applePie(){
 			
 		}
 
-		public static void applePie(){
-			
-		}
-
-		public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException {
 			Scanner input = new Scanner(System.in);
 			// Main.start("urmom");
 			mainMenu();
@@ -190,5 +308,4 @@ class Main {
 			
 			
 		}
-	}
-			
+}
